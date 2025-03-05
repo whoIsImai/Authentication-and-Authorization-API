0925fa921d709fae751a3226b0f75981854afe60
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-03-05 12:31:50 +0200

    email change since it is unqiue

--- tests/user.test.js
+++ tests/user.test.js
@@ -20,7 +20,7 @@ describe('all the activities the user can perform on different routes', () => {
             .send({
                 fname: 'John',
                 lname: 'Doe',
-                email: 'email10',
+                email: 'email11',
                 password: 'password'
             })
             console.log('Response Status:', resp.status);
